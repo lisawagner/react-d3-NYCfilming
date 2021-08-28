34b94b7e983d958a4839e5b6d902e0bc8ca6d14a
Lisa Wagner <lisawagner.online@gmail.com>
2021-08-28 12:01:11 -0700

    refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { LineChart } from "./components/LineChart/LineChart";
 // import { BarGraph } from "./components/BarGraph/BarGraph";
 import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <LineChart />
       {/* <BarGraph /> */}
     </div>
   );
-}
+};
 
 export default App;
