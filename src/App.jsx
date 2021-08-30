d23c424ee8851f8795e64aeec808d717f2782c72
Lisa Wagner <lisawagner.online@gmail.com>
2021-08-29 22:10:38 -0700

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { LineChart } from "./components/LineChart/LineChart";
 import { Sample } from "./components/Sample/Sample";
-import { BarChart } from "./components/BarChart/BarChart";
+// import { BarChart } from "./components/BarChart/BarChart";
 // import { BarGraph } from "./components/BarGraph/BarGraph";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
-      {/* <LineChart />
-      <Sample /> */}
-      <BarChart />
+      <LineChart />
+      <Sample />
+      {/* <BarChart /> */}
       {/* <BarGraph /> */}
     </div>
   );
