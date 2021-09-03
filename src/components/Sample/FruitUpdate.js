e7e659f2d736b3a357c96ac2c52fd19f01784de8
Lisa Wagner <lisawagner.online@gmail.com>
2021-09-03 12:47:25 -0700

    testing

--- src/components/Sample/FruitUpdate.js
+++ src/components/Sample/FruitUpdate.js
@@ -14,19 +14,36 @@ export const FruitUpdate = () => {
     const width = 960;
     const height = 500;
 
+    const svgElement = d3
+      .select(svgRef.current)
+      .attr("width", width)
+      .attr("height", height);
+
+    // render logic
+    const renderViz = (svgElement, { fruits }) => {
+      svgElement
+        .selectAll("circle") // makes empty selection
+        .data(fruits) //creates and returns data-join with your data array(fruits)
+        .enter() // computes the () selection of circles with data elements
+        .append("circle")
+        .attr("fill", "red")
+        .attr("cx", (d, i) => i * 120 + 60)
+        .attr("cy", height / 2)
+        .attr("r", 50);
+
+      svgElement.selectAll("circle").data(fruits).exit().remove();
+    };
+
     const makeFruit = (type) => ({ type });
     const fruits = d3.range(5).map(() => makeFruit("apple"));
 
-    const svg = d3
-      .select(svgRef.current)
-      .selectAll("circle")
-      .data(fruits)
-      .enter()
-      .append("circle")
-      .attr("fill", "red")
-      .attr("cx", (d, i) => i * 120 + 60)
-      .attr("cy", height / 2)
-      .attr("r", 50);
+    renderViz(svgElement, { fruits });
+
+    setTimeout(() => {
+      // state manipulation logic (eat an apple)
+      fruits.pop();
+      renderViz(svgElement, { fruits });
+    }, 1000);
   }, []);
 
   return (
